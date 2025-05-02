bfd651831cd6b85bbdacc3a9437a5dd889bfe573
emgr.gonzalez.r@gmail.com <emgr.gonzalez.r@gmail.com>
2025-05-02 01:25:58 -0500

    Modificaciones productos

--- nuevaPapeleria2/Main.java
+++ nuevaPapeleria2/Main.java
@@ -80,7 +80,7 @@ public class Main {
                 productoEncontrado.getPrecio(), productoEncontrado.getPorcentajeGanacia(),
                 productoEncontrado.getStock()+cantidad);*/
                 
-                nuevoinventario.registrarSalida();
+               
                break;
             case 4:
                System.out.print("Ingrese el nombre del cliente: ");
@@ -108,94 +108,30 @@ public class Main {
             case 5:
                System.out.print("Ingrese el ID del producto: ");
                int idModificar = scanner.nextInt();
-               System.out.print("Ingrese el nuevo precio del producto: ");
-               int precioNuevo = scanner.nextInt();
-               System.out.print("Ingrese el nuevo porcentaje de ganancia del producto: ");
-               double porcentajeNuevo = scanner.nextDouble();
-               System.out.print("Ingrese el nuevo stock del producto: ");
-               int stockNuevo = scanner.nextInt();
-               /*
-                * nuevoinventario.modificarProducto(idModificar, precioNuevo,
-                * porcentajeNuevo,stockNuevo);
-                * System.out.println("Producto modificado con exito");
-                * System.out.println("La nueva ganancia para este producto es de "
-                * + nuevoinventario.mostrarganancia(idModificar));
-                */
-               break;
-            case 6:
-               salir = true;
-               break;
-            default:
-               System.out.println("Opcion no valida.");
-         }
-         switch (opcion) {
-            case 1:
-               InfoConsola.mostraInfoInvetario(nuevoinventario);
-               break;
-            case 2:
-               System.out.print("Ingrese el ID del producto: ");
-               int id = scanner.nextInt();
-               scanner.nextLine();
-               System.out.print("Ingrese el nombre del producto: ");
-               String nombre = scanner.nextLine();
-               System.out.print("Ingrese el precio del producto: ");
-               int precio = scanner.nextInt();
-               System.out.print("Ingrese el procentaje de ganancia del producto: ");
-               double porcentaje = scanner.nextDouble();
-               System.out.print("Ingrese el stock del producto: ");
-               int stock = scanner.nextInt();
-               // Producto producto = new Producto(id, nombre, precio, porcentaje, stock);
-               // nuevoinventario.agregarProducto(producto);
-               System.out.println("Producto agregado al inventario.");
-               break;
-            case 3:
-               System.out.print("Ingrese el ID del producto: ");
-               int idBuscar = scanner.nextInt();
-               System.out.print("Ingrese la cantidad del producto retirado: ");
-               int cantidadRetirada = scanner.nextInt();
-               Producto 
-               /*
-                * Producto productoEncontrado =nuevoinventario.buscarProducto(idVendido);
-                * Venta venta = new Venta(cliente, productoEncontrado, cantidad);
-                * venta.mostrarVenta();
-                * nuevoinventario.modificarProducto(productoEncontrado.getId(),
-                * productoEncontrado.getPrecio(), productoEncontrado.getPorcentajeGanacia(),
-                * productoEncontrado.getStock()+cantidad);
-                */
-                nuevoinventario.registrarSalida();
-               break;
-            case 4:
-               System.out.print("Ingrese el nombre del cliente: ");
-               String nombreClienteEntrada = scanner.nextLine();
-               System.out.print("Ingrese el telefono del cliente: ");
-               String telefonoEntrada = scanner.nextLine();
-               Cliente clienteEntrada = new Cliente(nombreClienteEntrada, telefonoEntrada);
-               System.out.print("Ingrese el Id del producto vendido: ");
-               int idVendidoEntrada = scanner.nextInt();
-               System.out.print("Ingrese la cantidad: ");
-               int cantidadEntrada = scanner.nextInt();
-               /*
-                * Producto productoEncontradoEntrada
-                * =nuevoinventario.buscarProducto(idVendidoEntrada);
-                * Abastecimiento abastecimiento = new Abastecimiento(clienteEntrada,
-                * productoEncontradoEntrada, cantidadEntrada);
-                * abastecimiento.mostrarAbastecimiento();
-                * nuevoinventario.modificarProducto(productoEncontradoEntrada.getId(),
-                * productoEncontradoEntrada.getPrecio(),
-                * productoEncontradoEntrada.getPorcentajeGanacia(),
-                * productoEncontradoEntrada.getStock()-cantidadEntrada);
-                */
+               System.out.print("Ingrese el precio costo: ");
+               double precioCostoModificar = scanner.nextDouble();
+               System.out.print("Ingrese el nuevo Margen de ganancia: ");
+               float margenGananciaModificar = scanner.nextFloat();
+               System.out.print("Ingrese el nuevo precio de venta: ");
+               double precioVentaModificar = scanner.nextDouble();
+               System.out.print("Ingrese valor de ganancia de venta: ");
+               double valorGananaciaModificar = scanner.nextDouble();
+               System.out.print("Ingrese la cantidad actual: ");
+               int cantidadActualModificar = scanner.nextInt();
+               Producto productoModificado = null;
+               for (Producto producto : itemsProductos) {
+                  if (producto.getId()==idModificar) {
+                     productoModificado = producto;
+                  }
+               }
+               ItemInventario itemInventarioModificar = nuevoinventario.buscar(productoModificado);
+               itemInventarioModificar.setPrecioCosto(precioCostoModificar);
+               itemInventarioModificar.setMargenGanacia(margenGananciaModificar);
+               itemInventarioModificar.setPrecioVenta(precioVentaModificar);
+               itemInventarioModificar.setValorGananacia(valorGananaciaModificar);
+               itemInventarioModificar.setCantidadActual(cantidadActualModificar);
+               System.out.println("producto modificado con exito");
 
-               break;
-            case 5:
-               System.out.print("Ingrese el ID del producto: ");
-               int idModificar = scanner.nextInt();
-               System.out.print("Ingrese el nuevo precio del producto: ");
-               int precioNuevo = scanner.nextInt();
-               System.out.print("Ingrese el nuevo porcentaje de ganancia del producto: ");
-               double porcentajeNuevo = scanner.nextDouble();
-               System.out.print("Ingrese el nuevo stock del producto: ");
-               int stockNuevo = scanner.nextInt();
                /*
                 * nuevoinventario.modificarProducto(idModificar, precioNuevo,
                 * porcentajeNuevo,stockNuevo);
